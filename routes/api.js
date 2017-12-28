dd5fcbf680718a04e9674df7aacf7be8a8d738bd
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2017-12-28 08:41:52 +0100

    Better API error handling

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,8 @@ router.get('/balances', function (request, response, next) {
   bittrex.getbalances((data) => {
     if (data.success) {
       response.json(data.result)
+    } else {
+      response.status(500).json(data)
     }
   })
 })
@@ -25,6 +27,8 @@ router.get('/orderhistory', function (request, response, next) {
   bittrex.getorderhistory({}, (data) => {
     if (data.success) {
       response.json(data.result)
+    } else {
+      response.status(500).json(data)
     }
   })
 })
@@ -33,6 +37,8 @@ router.get('/withdrawalhistory', function (request, response, next) {
   bittrex.getwithdrawalhistory({}, (data) => {
     if (data.success) {
       response.json(data.result)
+    } else {
+      response.status(500).json(data)
     }
   })
 })
@@ -41,6 +47,8 @@ router.get('/deposithistory', function (request, response, next) {
   bittrex.getdeposithistory({}, (data) => {
     if (data.success) {
       response.json(data.result)
+    } else {
+      response.status(500).json(data)
     }
   })
 })
@@ -49,6 +57,8 @@ router.get('/openorders', function (request, response, next) {
   bittrex.getopenorders({}, (data) => {
     if (data.success) {
       response.json(data.result)
+    } else {
+      response.status(500).json(data)
     }
   })
 })
@@ -62,24 +72,29 @@ router.get('/market/cancel', function (request, response, next) {
   }
 
   bittrex.cancel({ uuid: uuid }, (data) => {
-    if (data && data.success) {
+    if (data.success) {
       response.json(data.result)
     } else {
-      response.status(500).json({'message': data})
+      response.status(500).json(data)
     }
   })
 })
 
-// TODO: do with websocker
+// TODO: do with websocket
 router.get('/ticker', function (request, response, next) {
   bittrex.getticker({}, ticker => {
     response.json(ticker)
   })
 })
 
+// TODO: do with websocket
 router.get('/marketsummaries', function (request, response, next) {
   bittrex.getmarketsummaries((data, error) => {
-    response.json(data.result)
+    if (data.success) {
+      response.json(data.result)
+    } else {
+      response.status(500).json(data)
+    }
   })
 })
 
