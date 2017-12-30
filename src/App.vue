557fbf4d06cef7f51afb2d0074f7d1234c734563
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2017-12-30 12:33:45 +0100

    Set proper default text alignment

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,6 @@ body {
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   padding-top: 80px;
 }
