70f627bc98165942ddeee8843ff2571529063559
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-01-01 19:12:35 +0100

    Show legend at history

--- src/components/History.vue
+++ src/components/History.vue
@@ -16,6 +16,13 @@
       </div>
     </header>
     <div class="history__body">
+      <div class="history__legend">
+        <div class="history__legend-symbol">Market</div>
+        <div class="history__legend-amount">Amount</div>
+        <div class="history__legend-profit">Profit</div>
+        <div class="history__legend-type">Type</div>
+      </div>
+
       <Order v-if="historyOrders" v-for="order in historyOrders" :key="order.OrderUuid" :order="order"></Order>
       <div v-if="showLoadingIndicator" class="history__empty">
         <p>Loading your order history...</p>
@@ -48,7 +55,7 @@ export default {
     },
     showLoadingIndicator () {
       return !this.historyOrders.length && this.isLoading
-    },
+    }
   }
 }
 </script>
@@ -93,6 +100,38 @@ export default {
     opacity: 0.5;
     font-weight: normal;
     text-align: center;
+  }
+
+  .history__legend {
+    display: flex;
+    width: 100%;
+    padding: 10px 15px;
+    font-weight: bold;
+
+    .history__legend-symbol {
+      flex-basis: 90px;
+      flex-shrink: 0;
+    }
+
+    .history__legend-amount {
+      flex-grow: 1;
+      max-width: 100%;
+      width: 100%;
+    }
+
+    .history__legend-profit {
+      flex-basis: 70px;
+      flex-shrink: 0;
+      text-align: right;
+      padding-right: 10px;
+    }
+
+    .history__legend-type {
+      flex-basis: 75px;
+      flex-shrink: 0;
+      text-align: left;
+      padding-right: 35px;
+    }
   }
 }
 </style>
