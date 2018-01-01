8dfeeb2621ec69025ff5ec7b110646c514fc8e86
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-01-01 19:13:39 +0100

    Properly get market data

--- src/pages/Markets.vue
+++ src/pages/Markets.vue
@@ -3,50 +3,27 @@
 
     <header class="page-header">
       <h2 class="page-header__title">Markets</h2>
-      <!-- <div class="page-header__control">
-        <div class="select-box">
-          <select name="order">
-            <option disabled selected>Order by...</option>
-            <option>Name</option>
-            <option>24hr vol</option>
-            <option>last price</option>
-            <option>24hr high</option>
-            <option>24hr low</option>
-          </select>
-        </div>
-      </div> -->
     </header>
 
     <ul class="tabs">
-      <li class="tabs__item"><router-link to="/markets" class="tabs__item-link" exact  @click.native="setSelectedMarket('all')">All <span>({{ marketCount('all') }})</span></router-link></li>
+      <li class="tabs__item"><router-link to="/markets" class="tabs__item-link" exact  @click.native="setSelectedMarket(null)">All <span>({{ marketCount('all') }})</span></router-link></li>
       <li class="tabs__item"><router-link to="/markets/BTC" class="tabs__item-link" @click.native="setSelectedMarket('BTC')">BTC <span>({{ marketCount('BTC') }})</span></router-link></li>
       <li class="tabs__item"><router-link to="/markets/ETH" class="tabs__item-link" @click.native="setSelectedMarket('ETH')">ETH <span>({{ marketCount('ETH') }})</span></router-link></li>
       <li class="tabs__item"><router-link to="/markets/USD" class="tabs__item-link" @click.native="setSelectedMarket('USD')">USD <span>({{ marketCount('USD') }})</span></router-link></li>
     </ul>
 
-    <div class="search-box">
-      <input type="search" name="search" v-model="searchQuery" placeholder="Search markets..." />
-      <!-- <Button class="search-box__control" v-if="searchQuery" :className="'link'" :label="'Clear'" @click.native="clearSearch()"></Button> -->
-    </div>
+    <router-view></router-view>
 
-    <Markets :filteredMarkets="filteredMarkets" :searchQuery="searchQuery"></Markets>
   </div>
 </template>
 
 <script>
-import Button from '@/components/Button'
-import Markets from '@/components/Markets'
-import store from '../store'
-
 export default {
   name: 'MarketsPage',
   props: ['selectedTab'],
-  components: {
-    Button,
-    Markets
-  },
   created () {
     console.log('created markets page')
+    this.$store.dispatch('balances/getAll') // Because we want to see what currency we have on our balance
   },
   data () {
     return {
@@ -57,50 +34,6 @@ export default {
   computed: {
     selectedMarket () {
       return this.$cookie.get('selectedMarket')
-    },
-    searchQueryInLowerCase () {
-      return (this.searchQuery ? this.searchQuery.toLowerCase().trim() : null)
-    },
-    filteredMarkets () {
-      const currency = this.$route.params.currency
-
-      if (currency === 'BTC') {
-        const btcMarkets = this.$store.getters['markets/allBtcMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
-        if (this.searchQueryInLowerCase) {
-          return btcMarkets.filter(market => {
-            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
-          })
-        } else {
-          return btcMarkets
-        }
-      } else if (currency === 'ETH') {
-        const ethMarkets = this.$store.getters['markets/allEthMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
-        if (this.searchQueryInLowerCase) {
-          return ethMarkets.filter(market => {
-            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
-          })
-        } else {
-          return ethMarkets
-        }
-      } else if (currency === 'USD') {
-        const usdMarkets = this.$store.getters['markets/allUsdMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
-        if (this.searchQueryInLowerCase) {
-          return usdMarkets.filter(market => {
-            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
-          })
-        } else {
-          return usdMarkets
-        }
-      } else {
-        const allMarkets = this.$store.getters['markets/allMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
-        if (this.searchQueryInLowerCase) {
-          return allMarkets.filter(market => {
-            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
-          })
-        } else {
-          return allMarkets
-        }
-      }
     }
   },
   methods: {
@@ -115,16 +48,15 @@ export default {
         return this.$store.getters['markets/allMarkets'].length
       }
     },
-    clearSearch () {
-      this.searchQuery = null
-    },
-    setSelectedMarket (currency) {
-      this.$cookie.set('selectedMarket', currency)
+    setSelectedMarket (market) {
+      if (market) {
+        this.$cookie.set('selectedMarket', market)
+        this.$store.commit('markets/setSelectedMarket', market)
+      } else {
+        this.$cookie.delete('selectedMarket')
+        this.$store.commit('markets/setSelectedMarket', '')
+      }
     }
-  },
-  beforeRouteEnter (to, from, next) {
-    store.dispatch('balances/getAll') // Because we want to see what currency we have on our balance
-    next()
   }
 }
 </script>
@@ -208,33 +140,6 @@ export default {
       z-index: 1;
       font-weight: bold;
     }
-  }
-}
-
-.search-box {
-  padding: 15px;
-  position: relative;
-
-  input[type="search"] {
-    border: 1px #DFE1E3 solid;
-    padding: 0 15px;
-    background-color: #fff;
-    border-radius: 3px;
-    color: #000;
-    font-size: 1.4rem;
-    line-height: 4rem;
-    height: 4rem;
-    width: 100%;
-    display: block;
-    outline: none;
-    margin: 0;
-    appearance: none;
-  }
-
-  .search-box__control {
-    position: absolute;
-    top: 0;
-    right: 0;
   }
 }
 
---
+++ src/pages/MarketsCurrency.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div class="search-box">
+      <input type="search" name="search" v-model="searchQuery" placeholder="Search markets..." />
+    </div>
+    <Markets :filteredMarkets="filteredMarkets" :searchQuery="searchQuery"></Markets>
+  </div>
+</template>
+
+<script>
+import Markets from '@/components/Markets'
+
+export default {
+  name: 'MarketsCurrencyPage',
+  components: {
+    Markets
+  },
+  created () {
+    console.log('created markets currency page')
+  },
+  data () {
+    return {
+      searchQuery: null,
+      selectedCurrency: null
+    }
+  },
+  computed: {
+    routeCurrency () {
+      return this.$route.params.currency
+    },
+    selectedMarket () {
+      return this.$cookie.get('selectedMarket')
+    },
+    searchQueryInLowerCase () {
+      return (this.searchQuery ? this.searchQuery.toLowerCase().trim() : null)
+    },
+    filteredMarkets () {
+      const currency = this.$route.params.currency
+
+      if (currency === 'BTC') {
+        const btcMarkets = this.$store.getters['markets/allBtcMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
+        if (this.searchQueryInLowerCase) {
+          return btcMarkets.filter(market => {
+            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
+          })
+        } else {
+          return btcMarkets
+        }
+      } else if (currency === 'ETH') {
+        const ethMarkets = this.$store.getters['markets/allEthMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
+        if (this.searchQueryInLowerCase) {
+          return ethMarkets.filter(market => {
+            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
+          })
+        } else {
+          return ethMarkets
+        }
+      } else if (currency === 'USD') {
+        const usdMarkets = this.$store.getters['markets/allUsdMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
+        if (this.searchQueryInLowerCase) {
+          return usdMarkets.filter(market => {
+            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
+          })
+        } else {
+          return usdMarkets
+        }
+      } else {
+        const allMarkets = this.$store.getters['markets/allMarkets'].sort((a, b) => b.BaseVolume - a.BaseVolume)
+        if (this.searchQueryInLowerCase) {
+          return allMarkets.filter(market => {
+            return market.MarketName.toLowerCase().includes(this.searchQueryInLowerCase)
+          })
+        } else {
+          return allMarkets
+        }
+      }
+    }
+  },
+  methods: {
+    marketCount (currency) {
+      if (currency === 'BTC') {
+        return this.$store.getters['markets/allBtcMarkets'].length
+      } else if (currency === 'ETH') {
+        return this.$store.getters['markets/allEthMarkets'].length
+      } else if (currency === 'USD') {
+        return this.$store.getters['markets/allUsdMarkets'].length
+      } else {
+        return this.$store.getters['markets/allMarkets'].length
+      }
+    },
+    clearSearch () {
+      this.searchQuery = null
+    },
+    setSelectedMarket (currency) {
+      this.$cookie.set('selectedMarket', currency)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  padding: 15px;
+  position: relative;
+
+  input[type="search"] {
+    border: 1px #DFE1E3 solid;
+    padding: 0 15px;
+    background-color: #fff;
+    border-radius: 3px;
+    color: #000;
+    font-size: 1.4rem;
+    line-height: 4rem;
+    height: 4rem;
+    width: 100%;
+    display: block;
+    outline: none;
+    margin: 0;
+    appearance: none;
+  }
+
+  .search-box__control {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+</style>
