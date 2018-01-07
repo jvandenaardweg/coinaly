c5eb51e94c487d3fe8dbb0e138042cfc2bc5cd1c
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-01-07 16:38:16 +0100

    Added Label tests

---
+++ test/unit/specs/Label.spec.js
@@ -0,0 +1,31 @@
+import { mount } from 'vue-test-utils'
+import Label from '@/components/Label'
+
+describe('Button.vue', () => {
+  it('is a Vue instance', () => {
+    const wrapper = mount(Label)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('should render the correct text', () => {
+    const testText = 'Buy'
+    const wrapper = mount(Label, {
+      propsData: {
+        text: testText
+      }
+    })
+    expect(wrapper.props().text).toBe(testText)
+    expect(wrapper.text()).toBe(testText)
+  })
+
+  it('should render the correct color class', () => {
+    const testColor = 'red'
+    const wrapper = mount(Label, {
+      propsData: {
+        color: testColor
+      }
+    })
+    expect(wrapper.props().color).toBe(testColor)
+    expect(wrapper.classes()).toContain(`label--${testColor}`)
+  })
+})
