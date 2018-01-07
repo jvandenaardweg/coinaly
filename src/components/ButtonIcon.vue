b72f0921a931757f2c1aa6595106d32d5ab79d2a
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-01-07 15:43:28 +0100

    Added ButtonIcon tests

--- src/components/ButtonIcon.vue
+++ src/components/ButtonIcon.vue
@@ -17,7 +17,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .button-icon {
   display: inline-block;
---
+++ test/unit/specs/ButtonIcon.spec.js
@@ -0,0 +1,35 @@
+import { mount } from 'vue-test-utils'
+import ButtonIcon from '@/components/ButtonIcon'
+
+describe('Button.vue', () => {
+  it('is a Vue instance', () => {
+    const wrapper = mount(ButtonIcon)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('should render the correct label', () => {
+    const testLabelText = 'Test button'
+    const wrapper = mount(ButtonIcon, {
+      propsData: {
+        label: testLabelText
+      }
+    })
+    expect(wrapper.props().label).toBe(testLabelText)
+    expect(wrapper.text()).toBe(testLabelText)
+  })
+
+  it('should render the correct SVG icon', () => {
+    const testIcon = 'chart'
+    const wrapper = mount(ButtonIcon, {
+      propsData: {
+        icon: testIcon
+      }
+    })
+    const iconElement = wrapper.find('.button-icon__icon')
+    const iconElementSvg = iconElement.find('svg')
+
+    expect(wrapper.props().icon).toBe(testIcon)
+    expect(iconElement.exists()).toBe(true)
+    expect(iconElementSvg.exists()).toBe(true)
+  })
+})
